30115ea44e0eef9b1dda2a7c4032ab685cbafd0e
d smitty <deriksmitty@gmail.com>
2021-10-21 09:42:22 -0500

    Add project files.

---
+++ ModChallenge/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sumS=0;
+            double squareS = 0;
+            double dif;
+            double cr;
+            int ans;
+            
+            // sum of squares
+            for(int i = 1; i <= 10; i++)
+            {
+                // sum of squares (1^2 + 2^2 + 3^2...)
+                
+
+                // square of sums (1+2+3...+10)^2
+
+            }
+
+            // find the difference between the 2 sums
+
+
+            // find the cubed root of the difference
+
+
+            // is that # odd or even?
+
+
+        }
+    }
+}
